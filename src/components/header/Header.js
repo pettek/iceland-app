4c6ee475fec6ac71ba44bcfbc4a3cbd66c6b8fcc
Piotr Olbrot <Piotr.Olbrot@hemmersbach.com>
2018-03-29 13:46:35 +0200

    Create navigation bar with NavLinks

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import './Header.css';
+import { NavLink }          from 'react-router-dom';
 
 export default class Header extends Component {
   render () {
-    return <h1>Header</h1>;
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                exact to="/"
+                activeClassName="selected"
+              >Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact to="/concerts"
+                activeClassName="selected"
+              >Concerts</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
