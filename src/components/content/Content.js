5f423ba5d747d18da968ce826fb55ee4ed5d1a2e
Piotr Olbrot <Piotr.Olbrot@hemmersbach.com>
2018-03-29 13:48:20 +0200

    Add switch between landing page and concert list

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './Content.css';
-import Concerts from '../concerts/Concerts';
+import ConcertList          from '../concert-list/ConcertList';
+import LandingPage          from '../landing-page/LandingPage';
+import { Switch, Route }           from 'react-router-dom';
 
 export default class Content extends Component {
   render () {
     return <div>
-      <Concerts/>
+      <Switch>
+        <Route exact path="/" render={() => <LandingPage/>} />
+        <Route exact path="/concerts" render={() => <ConcertList/> } />
+      </Switch>
     </div>;
   }
 }
