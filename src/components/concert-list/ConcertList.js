9054859dd0db6422b767b756b695dc195e887245
Piotr Olbrot <Piotr.Olbrot@hemmersbach.com>
2018-03-29 13:51:37 +0200

    Add components responsible for displaying concerts

---
+++ src/components/concert-list/ConcertList.js
@@ -0,0 +1,37 @@
+import React, { Component }     from 'react';
+import './ConcertList.css';
+import { fetchConcertsFromApi } from '../../actions';
+import { connect }              from 'react-redux';
+import Concert                  from '../concert/Concert';
+
+const mapStateToProps = state => ({
+  concerts: state.concerts
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchConcertsFromApi: () => dispatch(fetchConcertsFromApi())
+  };
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class ConcertList extends Component {
+  componentDidMount () {
+    this.props.fetchConcertsFromApi();
+  }
+
+  render () {
+    const concerts = this.props.concerts.map((concert, index) => (
+      <Concert key={index} details={concert}/>
+    ));
+
+    return (
+      <div>
+        <h2>
+          Concerts
+        </h2>
+        {concerts}
+      </div>
+      );
+  }
+}
