ba36aeea1b4eaad8e603f5fe7fad2b5c4926d8a9
Piotr Olbrot <Piotr.Olbrot@hemmersbach.com>
2018-04-04 12:24:26 +0200

    Add link to Ride component

--- src/components/carpooling/ride/Ride.js
+++ src/components/carpooling/ride/Ride.js
@@ -3,11 +3,14 @@ import './Ride.css';
 
 export default class Ride extends Component {
   render () {
-    const {from, to, date, time} = this.props.details;
+    const {link, from, to, date, time} = this.props.details;
     return <div className="ride-card row">
-      <div className="col-md-7">
+      <div className="col-md-6">
         <strong>{from}</strong> <i className="fa fa-arrow-circle-o-right" aria-hidden="true"/>&nbsp;
         <strong>{to}</strong> &nbsp;
+      </div>
+      <div className="col-md-1">
+        <a href={link}><i className="fa fa-external-link" aria-hidden="true"/></a>
       </div>
       <div className="col-md-3">
         <i className="fa fa-calendar-o" aria-hidden="true"/>&nbsp;{date} &nbsp;
